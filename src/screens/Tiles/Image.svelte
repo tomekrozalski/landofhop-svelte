41bea2c433bdf6aeedb79ddfc164dac4dca7ff9e
Tomek Rozalski <alium@daucusetanser.pl>
2019-10-20 21:29:37 +0200

    [Tiles] Make webpSupport store readable, not writable. Learning more about stores

--- src/screens/Tiles/Image.svelte
+++ src/screens/Tiles/Image.svelte
@@ -1,4 +1,5 @@
 <script>
+  import isBoolean from "lodash/isBoolean";
   import { language, webpSupport } from "../../utils/store";
   import { servers } from "../../utils/constants";
   import { getNameByLanguage } from "../../utils/helpers";
@@ -23,7 +24,7 @@
     language
   });
 
-  const coverPath = `${servers.images}${brandBadge}/${badge}/${shortId}/cover/${
+  $: coverPath = `${servers.images}${brandBadge}/${badge}/${shortId}/cover/${
     $webpSupport ? "webp" : "jpg"
   }`;
 </script>
@@ -42,14 +43,16 @@
   }
 </style>
 
-<img
-  alt={`${formattedName}, ${formattedBrand}`}
-  class:loaded
-  on:error
-  on:load
-  srcSet={`
-				${coverPath}/1x.${$webpSupport ? 'webp' : 'jpg'},
-				${coverPath}/2x.${$webpSupport ? 'webp' : 'jpg'} 2x,
-				${coverPath}/4x.${$webpSupport ? 'webp' : 'jpg'} 4x,
-			`}
-  src={`${coverPath}/1x.${$webpSupport ? 'webp' : 'jpg'}`} />
+{#if isBoolean($webpSupport)}
+  <img
+    alt={`${formattedName}, ${formattedBrand}`}
+    class:loaded
+    on:error
+    on:load
+    srcSet={`
+          ${coverPath}/1x.${$webpSupport ? 'webp' : 'jpg'},
+          ${coverPath}/2x.${$webpSupport ? 'webp' : 'jpg'} 2x,
+          ${coverPath}/4x.${$webpSupport ? 'webp' : 'jpg'} 4x,
+        `}
+    src={`${coverPath}/1x.${$webpSupport ? 'webp' : 'jpg'}`} />
+{/if}
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,18 @@
-import { writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
 import { languages } from './constants';
 
 export const isNavbar = writable(false);
 export const isLoginbar = writable(false);
 export const isLoggedIn = writable(false);
 export const language = writable(languages.site.pl);
-export const webpSupport = writable(false);
+
+export const webpSupport = readable(undefined, async set => {
+	if (!self.createImageBitmap) {
+		set(false);
+		return false;
+	};
+
+	const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+	const blob = await fetch(webpData).then(r => r.blob());
+	return createImageBitmap(blob).then(() => set(true), () => set(false));
+});
